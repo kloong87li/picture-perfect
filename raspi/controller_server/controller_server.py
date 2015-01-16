53e122a3ec133c6e1ec82a9e5a02e9dabab71eea
Connie Wang <connie.ru.wang@gmail.com>
2015-01-15 23:09:35 -0500

    initial server routes for camera settings

--- raspi/controller_server/controller_server.py
+++ raspi/controller_server/controller_server.py
@@ -21,7 +21,7 @@ def response_success(**kwargs):
 
 
 @app.route('/brightness', methods=['POST', 'GET'])
-def set_color():
+def set_brightness():
     if request.method == 'GET':
         # return camera brightness
         return response_success(value = camera.brightness)
@@ -35,6 +35,82 @@ def set_color():
             # set camera brightness and return success response
             camera.brightness = value
             return response_success()
+
+@app.route('/contrast', methods=['POST', 'GET'])
+def set_contrast():
+    if request.method == 'GET':
+        # return camera contrast
+        return response_success(value = camera.contrast)
+
+    elif reuqest.method == 'POST':
+        value = request.args['value']
+        if not value:
+            # no value param, return error response
+            return response_error()
+        else:
+            # set camera contrast and return success response
+            camera.contrast = value
+            return response_success()
+
+@app.route('/zoom', methods=['POST', 'GET'])
+def set_zoom():
+    if request.method == 'GET':
+        # return camera zoom
+        return response_success(value = camera.zoom)
+
+    elif reuqest.method == 'POST':
+        value = request.args['value']
+        if not value:
+            # no value param, return error response
+            return response_error()
+        else:
+            # set camera zoom and return success response
+            camera.zoom = value
+            return response_success()
+
+@app.route('/image_effect', methods=['POST', 'GET'])
+def set_image_effect():
+    if request.method == 'GET':
+        # return camera image_effect
+        return response_success(value = camera.image_effect)
+
+    elif reuqest.method == 'POST':
+        value = request.args['value']
+        if not value:
+            # no value param, return error response
+            return response_error()
+        else:
+            # set camera image_effect and return success response
+            # assuming a valid image_effect was given as a value
+            camera.image_effect = value
+            return response_success()
+
+@app.route('/flip', methods=['POST', 'GET'])
+def set_flip():
+    if request.method == 'GET':
+        # return camera flip
+        flip = (camera.vflip or camera.hflip)
+        return response_success(value = flip)
+
+    elif reuqest.method == 'POST':
+        value = request.args['value']
+        if not value:
+            # no value param, return error response
+            return response_error()
+        else:
+            # set camera flip and return success response
+            if (value == 'hflip'):
+                camera.hflip = not camera.hflip
+            else:
+                camera.vflip = not camera.vflip 
+            return response_success()
+
+
+@app.route('/capture', methods=['POST'])
+def capture():
+    # take picture and return success response
+    camera.capture('picture_perfect', 'jpeg')
+    return response_success()
 
 
 if __name__ == "__main__":
